64d47bdb554ca26112b4c18e75dde031e51775d8
blowfish339 <georgiablack2020@outlook.com>
2024-10-16 16:54:38 +0800

    lifespan

--- redis_app/main.py
+++ redis_app/main.py
@@ -3,21 +3,20 @@ from fastapi import FastAPI, Depends, HTTPException
 from contextlib import asynccontextmanager
 import uvicorn
 from schemas import User
+from aioredis import ConnectionPool
 
-# @asynccontextmanager
-# async def get_redis():
-#     try:
-#         db = await aioredis.from_url(url='redis://127.0.0.1', encoding='utf-8', decode_responses=True)
-#         yield db
-#     finally:
-#         await db.close()
+async def get_redis(app: FastAPI):
+    try:
+        app.state.redis = await aioredis.from_url(url='redis://127.0.0.1', encoding='utf-8', decode_responses=True)
+        yield
+    finally:
+        await app.state.redis.close()
 
-db = aioredis.from_url(url='redis://127.0.0.1', encoding='utf-8', decode_responses=True)
-app = FastAPI()
+app = FastAPI(lifespan=get_redis)
 
 @app.get('/users/{user_email}', response_model=User)
 async def get_users(user_email: str):
-    user_password = await db.get(user_email)
+    user_password = await app.state.redis.get(user_email)
     if not user_password:
         raise HTTPException(status_code=404, detail='User not found')
     user = User(email=user_email, password=user_password)
@@ -25,7 +24,7 @@ async def get_users(user_email: str):
 
 @app.post('/users/')
 async def create_user(user: User):
-    await db.set(user.email, user.password)
+    await app.state.redis.set(user.email, user.password)
     return {'message': 'User created successfully'}
 
 if __name__ == '__main__':
